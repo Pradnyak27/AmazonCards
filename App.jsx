27661153173cd8e896b1c149540ac0052669892a
Pradnya Kutwade <kutwadepradnya28@gmail.com>
2025-03-25 16:58:29 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import ProductTab from "./ProductTab";
+function App(){
+  let styles={marginLeft:"650px"}
+  return(
+    <div>
+      <h2 style={styles}>BlockBuster Deals | Shop Now</h2>
+      <ProductTab/>
+    </div>
+  );
+}
+
+export default App;
